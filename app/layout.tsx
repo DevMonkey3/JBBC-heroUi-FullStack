77ddcd788f5b7b36602491ee912b344128f5cd7f
Tashdid <Tashdid@gmaail.com>
2025-09-21 16:15:01 +0800

    feat: updated the company info

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,11 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.png",
+    icon: [
+      { url: "/favicon.png" },
+      { url: "/favicon.png", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png", 
   },
 };
 
--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Japan Bangla Bridge Company (JBBC) ",
+  description: "A great website for finding overseas talent.",
   navItems: [
     {
       label: "Home",
